eb910aef1fa5ae28fe9075cddf68ef6b31f88139
Gennie Harris <gennie.harris@singlebrook.com>
2014-10-23 19:24:40 -0400

    Create inital app

---
+++ songlist.js
@@ -0,0 +1,20 @@
+var songlist = angular.module("SonglistApp", ['ui.keypress']);
+
+songlist.controller("listController", ["$scope", "Songs", "searchFilter", function($scope, Songs, searchFilter) {
+  $scope.songList = Songs;
+
+  $scope.predicate = "title";
+
+  $scope.filterSongs = function(searchText) {
+    $scope.songList = searchFilter(Songs, searchText);
+  };
+}]);
+
+songlist.filter("search", function() {
+  return function(songList, text) {
+    return _.filter(songList, function(song) {
+      var re = new RegExp(text, 'i');
+      return !text || re.test(song.title) || re.test(song.artist);
+    });
+  };
+});
